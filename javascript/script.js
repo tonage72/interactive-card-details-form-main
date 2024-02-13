7112a99e1ec8528874d07952ed6c76b5b1bf4bbc
David Fleming <tonage72@gmail.com>
2024-02-13 18:40:26 -0500

    learning some javascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,26 @@
 const displayNumber = document.querySelector('.display-number');
 const inputNumber = document.querySelector('#input-number');
 
-inputNumber.addEventListener("keyup", (e) => {
+inputNumber.addEventListener("keydown", (e) => {
 
-	// const totalNumber = e.target.value.replaceALL(" ", "");
-	const totalNumber = e.target.value;
+console.log(e.key);
 
-	if (e.target.value.length > 14) {
-		e.target.value = totalNumber.replace(/(\d{4})(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3 $4");
-	} else if (e.target.value.length > 9) {
-		e.target.value = totalNumber.replace(/(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3");
-	} else if (e.target.value.length > 4) {
-		e.target.value = totalNumber.replace(/(\d{4})(\d{0,4})/, "$1 $2");
+	if (!e.target.value) {
+		displayNumber.textContent = "0000 0000 0000 0000";
 	} else {
-		displayNumber.innerHTML = totalNumber;
+		const totalNumber = e.target.value.replaceAll(" ", "");
+
+		if (e.target.value.length > 14) {
+			e.target.value = totalNumber.replace(/(\d{4})(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3 $4");
+			displayNumber.innerHTML = totalNumber.replace(/(\d{4})(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3 $4");
+		} else if (e.target.value.length > 9) {
+			e.target.value = totalNumber.replace(/(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3");
+			displayNumber.innerHTML = totalNumber.replace(/(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3");
+		} else if (e.target.value.length > 4) {
+			e.target.value = totalNumber.replace(/(\d{4})(\d{0,4})/, "$1 $2");
+			displayNumber.innerHTML = totalNumber.replace(/(\d{4})(\d{0,4})/, "$1 $2");
+		} else {
+			displayNumber.innerHTML = totalNumber;
+		}
 	}
 })
