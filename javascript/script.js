45ffebbe3e96cf6285ff97e21e1f31661e6dc21f
David Fleming <tonage72@gmail.com>
2024-02-12 19:31:28 -0500

    Working on spacing in input

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,18 @@
 const displayNumber = document.querySelector('.display-number');
-let inputNumber = document.getElementById('input-number');
+const inputNumber = document.querySelector('#input-number');
 
-inputNumber.addEventListener("keyup", updateCardNumber);
+inputNumber.addEventListener("keyup", (e) => {
 
-function updateCardNumber() {
-	let numberString = inputNumber.value.toString();
-	displayNumber.textContent = numberString.replace(/.{4}/g, '$& ');
-	inputNumber. = numberString.replace(/.{4}/g, '$& ');
-}
+	// const totalNumber = e.target.value.replaceALL(" ", "");
+	const totalNumber = e.target.value;
+
+	if (e.target.value.length > 14) {
+		e.target.value = totalNumber.replace(/(\d{4})(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3 $4");
+	} else if (e.target.value.length > 9) {
+		e.target.value = totalNumber.replace(/(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3");
+	} else if (e.target.value.length > 4) {
+		e.target.value = totalNumber.replace(/(\d{4})(\d{0,4})/, "$1 $2");
+	} else {
+		displayNumber.innerHTML = totalNumber;
+	}
+})
